fea26354aebf2d08cbeb32cd946b54a55e16c876
Alexander Markov <markov_av_77@mail.ru>
2024-09-09 07:04:22 +0400

    Made base file task 2_1

--- 2_1_erase_element.cpp
+++ 2_1_erase_element.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+vector <int> erase_element (vector<int> vec, int num_del)
+{
+    vector<int> new_vec;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] != num_del)
+        {
+            new_vec.push_back(vec[i]);
+        }
+    }
+    vec = new_vec;
+    return vec;
+}
 int main()
 {
     cout << "\033[2J\033[1;1H";
-    cout << "This program takes some numbers from user and get them back in reverse order\n";
+    cout << "This program takes some numbers from user, puts them all";
+    cout << "into array and suggests user to delete significant element from that array\n ";
     cout << "How many elements will be contain your array?\n";
     int n;
     cin >> n;
+    vector <int> vec (n);
+    cout << "Input elements...\n";
+    for (int i = 0; i < n; i++) {
+        cin >> vec [i];
+    }
+    cout << "Input element to delete\n";
+    int num_del;
+    cin >> num_del;
+    vec = erase_element(vec, num_del);
+    cout << "Result:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << vec[i] << "  ";
+    }
 }
