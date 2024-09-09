60a62cba034f5bb682d0481c85c9ddf6e1e6b0f9
Alexander Markov <markov_av_77@mail.ru>
2024-09-09 06:37:43 +0400

    Create file homework 2_1

---
+++ 2_1_erase_element.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cout << "\033[2J\033[1;1H";
+    cout << "This program takes some numbers from user and get them back in reverse order\n";
+    cout << "How many elements will be contain your array?\n";
+    int n;
+    cin >> n;
+}
